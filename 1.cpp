23686637cdffb81474dbb2fd79d9bf2d0dbe9461
ryucraftz <vaibhavsathe020>
2024-01-11 21:30:17 +0530

    two sum problem

--- 1.cpp
+++ 1.cpp
@@ -1,1 +1,23 @@
-mkfdk
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+    vector <int> twoSum(vector<int> &nums, int target){
+        int n = nums.size();
+        for (int i = 0; i < n-1; i++){
+            for (int j = i + 1;j<n; j++){
+                if (nums[i] + nums[j] == target){
+                    return{i,j};
+                }
+            }
+        }
+        return{}; 
+    }  
+
+};
+
+int main(){
+  int nums[] = {2,7,11,15};
+  int target = 9;
+};
